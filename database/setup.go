7a0bd660f34c5afcf63899ecef7ecfd593934ed7
laksanagusta <gustalaksanadika@gmail.com>
2023-01-18 22:01:01 +0700

    Remove get dotenv

--- database/setup.go
+++ database/setup.go
@@ -4,17 +4,11 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func SetupDatabase() *gorm.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	dsn := "root:" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DATABASE") + "?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
